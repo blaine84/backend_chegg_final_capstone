c0e8bed2e0b0aec4c123b7c864e25bedec0247b6
Blaine <blaine84@gmail.com>
2026-01-05 16:18:07 -0700

    fixed theaters route

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,7 +2,7 @@ const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(request, response) {
-  const data = await service.list(request);
+  const data = await service.list();
   response.json({ data });
 }
 
